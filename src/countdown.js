f103e8f0d6b9c0dd68600d4ae4c29cdca636349c
Omer Faruk APLAK <omeraplak@gmail.com>
2018-12-20 11:14:39 +0300

    indentation fix

--- src/countdown.js
+++ src/countdown.js
@@ -1,31 +1,31 @@
 import React, { useState, useRef, useEffect } from "react";
 
 export function Countdown(props) {
-    const intervalRef = useRef();
-    const [countDown, setCountdown] = useState(50);
+  const intervalRef = useRef();
+  const [countDown, setCountdown] = useState(50);
 
-    console.log("asda", countDown)
-    useEffect(() => {
-        console.log("useEffect", countDown)
-        const id = setInterval(function () {
-            const newSecond = countDown - 1;
+  console.log("asda", countDown)
+  useEffect(() => {
+    console.log("useEffect", countDown)
+    const id = setInterval(function () {
+      const newSecond = countDown - 1;
 
-            setCountdown(newSecond);
-        }, 1000);
+      setCountdown(newSecond);
+    }, 1000);
 
-        intervalRef.current = id;
+    intervalRef.current = id;
 
-        return () => {
-            clearInterval(intervalRef.current);
-        }
-    });
+    return () => {
+      clearInterval(intervalRef.current);
+    }
+  });
 
-    return (
-        <div>
-            <p>{countDown} seconds</p>
-            <button onClick={() => setCountdown(countDown + 10)}>
-                Add to 10 seconds
+  return (
+    <div>
+      <p>{countDown} seconds</p>
+      <button onClick={() => setCountdown(countDown + 10)}>
+        Add to 10 seconds
       		</button>
-        </div>
-    );
+    </div>
+  );
 }
